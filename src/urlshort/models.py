3b1209514b68bbdbeee08b3417d772573c207943
Akklarin <167417025+Akklarin@users.noreply.github.com>
2025-05-01 11:16:43 +0300

    _

--- src/urlshort/models.py
+++ src/urlshort/models.py
@@ -4,5 +4,6 @@ from src.database import Base
 
 class Urls(Base):
     __tablename__ = 'urls'
-    short_url = Column(String, primary_key=True, index=True)
-    original_url = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    short_url = Column(String, unique=True)
+    original_url = Column(String, nullable=False)

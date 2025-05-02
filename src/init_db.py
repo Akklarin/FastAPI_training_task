3a7fbc69c393e0ecf11a37d0941468ddb7807489
Akklarin <167417025+Akklarin@users.noreply.github.com>
2025-05-02 04:10:52 +0300

    _

--- src/init_db.py
+++
@@ -1,7 +0,0 @@
-from src.database import Base, engine
-from src.urlshort.models import Urls
-
-
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from src.api import main_router
 from src.urlshort.config import settings
-from src.init_db import init_db
 import uvicorn
 
 app = FastAPI(
@@ -11,11 +10,6 @@ app = FastAPI(
     )
 
 app.include_router(main_router)
-
-
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
 
 
 if __name__ == '__main__':
